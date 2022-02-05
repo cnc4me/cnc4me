973e6cdab59f9b4ae7242e95d85f17534f103211
Kevin Hill <kevinkhill@gmail.com>
2022-02-04 20:57:33 -0800

    adding some styling to the playground

--- playground/App.tsx
+++ playground/App.tsx
@@ -3,9 +3,18 @@ import React, { useEffect, useRef, useState } from "react";
 
 import { evaluate, interpreter } from "../src";
 
-export default function App() {
-  const example = `#1=[1+2]*3` + "\n".repeat(9);
+const example = [
+  "#1=5",
+  "#2=[#1+2]*3",
+  "#3=#2/#1",
+  "#4=[4*#2]-3",
+  "#5=[6+#3+#2]/[#4-#2]",
+  "",
+  "",
+  ""
+].join("\n");
 
+export default function App() {
   const editor = useRef();
   const [macros, setMacros] = useState<[number, number][]>([]);
 
@@ -36,20 +45,36 @@ export default function App() {
   }, [editor.current]);
 
   return (
-    <div className="flex flex-row h-screen bg-zinc-800">
-      <div className="flex flex-col w-1/3">
-        <div className="grow" ref={editor} />
+    <div className="flex flex-col bg-violet-900">
+      <div className="font-bold text-violet-300">
+        <h1 className="text-2xl py-2 pl-4">Fanuc Macro B Playground</h1>
       </div>
-      <div className="grow font-mono bg-zinc-900 p-5">
-        {macros.map(macro => {
-          return (
-            !isNaN(macro[1]) && (
-              <p key={macro[0]} className="text-xl text-indigo-600">
-                #{macro[0]}= {macro[1]}
-              </p>
-            )
-          );
-        })}
+      <div className="flex flex-row h-screen bg-neutral-800">
+        <div className="flex flex-col w-1/3">
+          <div className="grow" ref={editor} />
+        </div>
+        <div className="grow font-mono bg-neutral-900 p-5">
+          <table className="table-auto">
+            <thead>
+              <tr className="text-xl text-violet-400">
+                <th className="text-left w-32">Macro #</th>
+                <th className="text-left">Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {macros.map(macro => {
+                return (
+                  !isNaN(macro[1]) && (
+                    <tr key={macro[0]} className="text-xl text-violet-600">
+                      <td>#{macro[0]}</td>
+                      <td>{macro[1]}</td>
+                    </tr>
+                  )
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
