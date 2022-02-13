113374f923700b3f195ebd3ef04d0e439b0dc934
Kevin Hill <kevinkhill@gmail.com>
2022-02-12 20:15:34 -0800

    clearing the colors

--- src/monaco-dev/gcode-lang/gcode-dark.ts
+++ src/monaco-dev/gcode-lang/gcode-dark.ts
@@ -4,8 +4,8 @@ export const gcodeDarkTheme: MonacoThemeDef = {
   base: "vs-dark",
   inherit: true,
   rules: [
-    { token: "number", foreground: "#FFE38A" },
-    { token: "macro-var", foreground: "#F1AAFF" },
+    // { token: "number", foreground: "#FFE38A" },
+    // { token: "macro-var", foreground: "#F1AAFF" },
     // { token: "operators", foreground: "#cfcf4f" },
     { token: "comment", foreground: "#C09D70", fontStyle: "italic" }
     // { token: "custom-date", foreground: "008800" }
