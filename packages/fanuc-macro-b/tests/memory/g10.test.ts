28b2d9965d0efb4712140e5f914b94f5a5269834
Kevin Hill <kevinkhill@gmail.com>
2022-03-30 19:55:07 -0700

    tests for MacroMemory

--- packages/fanuc-macro-b/tests/memory/g10.test.ts
+++ packages/fanuc-macro-b/tests/memory/g10.test.ts
@@ -6,8 +6,9 @@ import { MacroMemory } from "../../src";
  * @example ```
  * |--------|------------|-------|------------|
  * | Length | Len. Comp. | Diam. | Diam Comp. |
- * |--------|------------|-------|------------| * |  L11   |   L10      |  L13  |  L12
- * |--------|------------|-------|-----------
+ * |--------|------------|-------|------------|
+ * |  L11   |   L10      |  L13  |  L12       |
+ * |--------|------------|-------|------------|
  *
  * L10 = Tool Length Compensation
  * L11 = Tool Length Geometry
@@ -18,26 +19,28 @@ import { MacroMemory } from "../../src";
 const mem = new MacroMemory();
 
 describe("testing the MacroMemory#g10()", () => {
-  describe("can set length offset registers", () => {
-    it("sets `T1` without error", () => {
-      mem.g10({ L: 10, P: 1, R: 0.002 });
-      mem.g10({ L: 11, P: 1, R: 5.1234 });
+  it("can set `T1` via G10 lines", () => {
+    mem.g10({ L: 10, P: 1, R: 0.002 });
+    mem.g10({ L: 11, P: 1, R: 5.1234 });
+    mem.g10({ L: 12, P: 1, R: -0.0005 });
+    mem.g10({ L: 13, P: 1, R: 0.375 });
 
-      expect(mem.read(10001)).toBe(0.002);
-      expect(mem.read(11001)).toBe(5.1234);
-    });
+    expect(mem.read(10001)).toBe(0.002);
+    expect(mem.read(11001)).toBe(5.1234);
+    expect(mem.read(12001)).toBe(-0.0005);
+    expect(mem.read(13001)).toBe(0.375);
   });
 
-  it("can set all offset registers for `T22` via g10 line.", () => {
-    mem.g10({ L: 11, P: 22, R: 8.6753 });
+  it("can set `T55` via G10 lines", () => {
+    mem.g10({ L: 10, P: 55, R: 0.001 });
+    mem.g10({ L: 11, P: 55, R: 1.2512 });
+    mem.g10({ L: 12, P: 55, R: 0.003 });
+    mem.g10({ L: 13, P: 55, R: 0.5 });
 
-    expect(mem.read(11022)).toBe(8.6753);
-  });
-
-  it("can set `T22` length offset via g10 line.", () => {
-    mem.g10({ L: 11, P: 22, R: 8.6753 });
-
-    expect(mem.read(11022)).toBe(8.6753);
+    expect(mem.read(10055)).toBe(0.001);
+    expect(mem.read(11055)).toBe(1.2512);
+    expect(mem.read(12055)).toBe(0.003);
+    expect(mem.read(13055)).toBe(0.5);
   });
 
   it.skip("can set `G54` work offsets via g10 line.", () => {
