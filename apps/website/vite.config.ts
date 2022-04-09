cae695db9c10c3296ed8ac90073ba2acef1ab2ff
Kevin Hill <kevinkhill@gmail.com>
2022-04-09 09:28:24 -0700

    fixing some refs and publish path

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   build: {
-    outDir: "../../dist/website"
+    outDir: "./dist"
   },
   plugins: [react(), tsconfigPaths()]
 });
