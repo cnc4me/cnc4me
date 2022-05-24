7f4160c8f167b0abe077bfb16eac53700740a212
Kevin Hill <kevinkhill@gmail.com>
2022-05-24 04:22:18 -0700

    removing a package for simplicity

--- packages/chevrotain-jest-extension/jest.config.js
+++
@@ -1,7 +0,0 @@
-const { jestConfig } = require("../../tools");
-const pkg = require("./package.json");
-
-module.exports = {
-  ...jestConfig(pkg),
-  setupFilesAfterEnv: ["jest-extended/all"]
-};
--- packages/fanuc-macro-b/tests/setup.ts
+++ packages/fanuc-macro-b/tests/setup.ts
@@ -1,3 +1,19 @@
 import "jest-extended";
 import "@cnc4me/jest-matchers";
-import "@cnc4me/chevrotain-jest-extension";
+
+import { IToken, tokenMatcher, TokenType } from "chevrotain";
+
+function toMatchToken(
+  this: jest.MatcherContext,
+  expected: IToken,
+  received: TokenType
+): jest.CustomMatcherResult {
+  const pass = tokenMatcher(expected, received);
+
+  return {
+    message: () => `expected ${expected.tokenType.name} to be ${received.name}`,
+    pass
+  };
+}
+
+expect.extend({ toMatchToken });
