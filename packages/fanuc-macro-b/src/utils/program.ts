efdf8fb0e6c4484a5f7dd4c5ae05a8ab65fec19b
Kevin Hill <kevinkhill@gmail.com>
2022-05-23 18:07:42 -0700

    fixing a type

--- packages/fanuc-macro-b/src/utils/program.ts
+++ packages/fanuc-macro-b/src/utils/program.ts
@@ -1,11 +1,12 @@
+import { IRecognitionException } from "chevrotain";
 import type { InsightCollection } from "../lib";
-import type { InterpretedProgram, ParsingErrors } from "../types";
+import type { InterpretedProgram } from "../types";
 import { createToolchain } from "./createToolchain";
 
 interface ProgramAnalisys {
   result: InterpretedProgram;
   insights: InsightCollection;
-  errors: ParsingErrors;
+  errors: IRecognitionException[];
 }
 
 /**
